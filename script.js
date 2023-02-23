44f8813be63d2c585d7d25f5cb4b0c8688553e35
Emma Stout <emma@primeacademy.io>
2023-02-23 15:17:32 -0600

    add event handlers and render function

--- script.js
+++ script.js
@@ -3,16 +3,77 @@ $(document).ready(onReady);
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+let fungusHP = 100;
+let heroAP = 100;
 
 function onReady() {
-    
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
-    
-    
+
+    $('.arcane-sceptre').on('click', handleSceptreAttack);
+    $('.entangle').on('click', handleEntangleAttack);
+    $('.dragon-blade').on('click', handleBladeAttack);
+    $('.star-fire').on('click', handleFireAttack);
+
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+    render();
+}
+
+function handleSceptreAttack() {
+    handleAttack('sceptre');
+}
+
+function handleEntangleAttack() {
+    handleAttack('entangle');
+}
+
+function handleBladeAttack() {
+    handleAttack('blade');
+}
+
+function handleFireAttack() {
+    handleAttack('fire');
+}
+
+function handleAttack(type) {
+    if (type === 'sceptre') {
+        fungusHP -= 14;
+        heroAP -= 12;
+        render();
+    }
+    if (type === 'entangle') {
+        fungusHP -= 9;
+        heroAP -= 23;
+        render();
+    }
+    if (type === 'blade') {
+        fungusHP -= 47;
+        heroAP -= 38;
+        render();
+    }
+    if (type === 'fire') {
+        fungusHP -= 25;
+        heroAP -= 33;
+        render();
+    }
+}
+
+function render() {
+    console.log('rendering!')
+    console.log({fungusHP})
+    console.log({ heroAP })
+    if (fungusHP < 0) {
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('dead');
+        fungusHP = 0;
+    } else if (heroAP < 0) {
+        $('.freaky-fungus').removeClass('walk');
+        $('.freaky-fungus').addClass('jump');
+        heroAP = 0;
+    }
+    $('.hp-text').empty();
+    $('.hp-text').append(`${fungusHP} AP`);
+    $('.ap-text').empty();
+    $('.ap-text').append(`${heroAP} AP`);
 }
